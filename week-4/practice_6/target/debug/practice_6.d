0e48b0ad6ca6098918b4428e157b8e145a350c5c
meymunahao <meymunah.olajobi@pau.edu.ng>
2022-11-05 06:54:24 +0100

    (My best commit yet)

---
+++ week-4/practice_6/target/debug/practice_6.d
@@ -0,0 +1,1 @@
+C:\Users\monaj\Documents\m.olajobiCSC101\week-4\practice_6\target\debug\practice_6.exe: C:\Users\monaj\Documents\m.olajobiCSC101\week-4\practice_6\src\main.rs
---
+++ week-4/practice_8/target/debug/practice_8.d
@@ -0,0 +1,1 @@
+C:\Users\monaj\Documents\m.olajobiCSC101\week-4\practice_8\target\debug\practice_8.exe: C:\Users\monaj\Documents\m.olajobiCSC101\week-4\practice_8\src\main.rs
